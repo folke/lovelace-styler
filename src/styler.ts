b9519b7032d649356cccfc9733a926bc01601186
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-02-08 16:31:09 +0100

    fix: properly update when rendering inside the card editor

--- src/styler.ts
+++ src/styler.ts
@@ -1,4 +1,4 @@
-import type { CSSResult, LitElement, ReactiveElement } from "lit"
+import type { CSSResult, ReactiveElement, LitElement } from "lit"
 
 type StylerConfig = {
   card?: boolean
@@ -6,7 +6,11 @@ type StylerConfig = {
   size?: number
 }
 
-type StylerState = StylerConfig & { parent_card?: boolean }
+type StylerState = StylerConfig & {
+  parent_card?: boolean
+  preview?: boolean
+  styleElement?: HTMLStyleElement
+}
 
 type StylerElement = LitElement & {
   config?: { styler?: StylerConfig }
@@ -21,17 +25,8 @@ let CARD_CSS = ""
 // Check if we need to apply styles to the element or any of its ancestors
 // This function will be called only once per element
 function check(node?: StylerElement): StylerState | undefined {
-  // When this is a top-level preview card, delete any custom style child elements from the shadow root
-  if (node?.parentElement?.tagName == "HUI-CARD-PREVIEW") {
-    node._styler = undefined
-    for (const child of node.shadowRoot?.children ?? []) {
-      if (child.tagName == "STYLE") {
-        child.remove()
-      }
-    }
-  }
-
-  if (!node || node._styler) return node?._styler
+  // Only re-apply styles if this is a new element, or we're in preview mode
+  if (!node || (node._styler && !node._styler.preview)) return node?._styler
 
   const parent = check(
     (node instanceof ShadowRoot ? node.host : node.parentElement ?? node.parentNode) as
@@ -39,8 +34,10 @@ function check(node?: StylerElement): StylerState | undefined {
       | undefined
   )
   node._styler = {
+    ...node._styler,
     ...node?._config?.styler,
     parent_card: parent?.card ?? parent?.parent_card,
+    preview: node?.tagName == "HUI-CARD-PREVIEW" || parent?.preview,
   }
   fix(node)
   return node._styler
@@ -50,18 +47,30 @@ function fix(node: StylerElement) {
   const config = node._styler
   if (!config) return
 
+  let css: string | undefined
+
   // Should we style this as a card?
-  if (config.card) {
-    const styleElement = document.createElement("style")
-    styleElement.innerHTML = CARD_CSS
-    node.shadowRoot?.appendChild(styleElement)
-  }
+  if (config.card) css = CARD_CSS
 
   // Do we have any custom styles to apply?
-  if (config.style) {
-    const styleElement = document.createElement("style")
-    styleElement.innerHTML = config.style
-    node.shadowRoot?.appendChild(styleElement)
+  if (config.style) css = (css ?? "") + config.style
+
+  if (css) {
+    config.styleElement ??= document.createElement("style")
+    if (config.styleElement.innerHTML !== css) config.styleElement.innerHTML = css
+    if (config.styleElement.parentNode !== node.shadowRoot) {
+      config.styleElement.remove()
+      node.shadowRoot?.appendChild(config.styleElement)
+    }
+  } else if (config.styleElement) {
+    config.styleElement.remove()
+    config.styleElement = undefined
+  }
+
+  if (config.preview && node.style) {
+    if (node.style.border == "none") node.style.border = ""
+    if (node.style.boxShadow == "none") node.style.boxShadow = ""
+    if (node.style.background == "none") node.style.background = ""
   }
 
   // Remove the card styles if either:
@@ -72,11 +81,9 @@ function fix(node: StylerElement) {
     node.tagName == "HA-CARD" &&
     (config.card === false || config.parent_card || config.parent_card === false)
   ) {
-    node.style.transition = "none"
     node.style.border = "none"
     node.style.boxShadow = "none"
     node.style.background = "none"
-    // node.style.transition = '';
   }
 }
 
